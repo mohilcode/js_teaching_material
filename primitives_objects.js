b707365ec23987f230ff0b6170cbc9c9ff815c3a
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-12 21:26:30 +0900

    Create primitives_objects.js

---
+++ primitives_objects.js
@@ -0,0 +1,20 @@
+// nn bb ss u
+
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("567")
+let e = "Harry"
+let f = Symbol("Hiii You")
+let g = undefined
+console.log(a, b, c, d, e, f, g);
+console.log(typeof c)
+
+const item ={
+	"Harry": true,
+	"Shubh": false,
+	"Lovish": 67,
+	"Rohan": undefined
+}
+console.log(item["Harry"]);
+console.log(item["aergrt"]); // will give undefined
