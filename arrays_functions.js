dc19f53f5b7ffd720912e967fdc98717b03444e8
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-12 20:41:14 +0900

    Create arrays_functions.js

---
+++ arrays_functions.js
@@ -0,0 +1,32 @@
+function wordBlanks(myNoun, myAdjective, myVerb, MyAdverb){
+	var result = "";
+	result += "The " + myAdjective + " " + myNoun + " " + myVerb + " " + "to the store" + " " + MyAdverb;
+	return result;
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"));
+
+var myArray = ["Quincy", 1];
+
+var nestedArray = [["Bulls", 23], ["White", 45]];
+console.log(nestedArray);
+
+var newArray = [50,60,70];
+
+var myData = newArray[0];
+
+newArray[2] = 69;
+
+newArray.push(90);
+console.log(newArray);
+
+var removed = newArray.pop();
+console.log(removed);
+
+var removed = newArray.shift();
+console.log(removed);
+
+newArray.unshift(420);
+console.log(newArray);
+
+var myList = [["cereal", 3], ["milk",2], ["bananas",3]]
