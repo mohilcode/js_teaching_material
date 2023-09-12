f5ab39aa7761d38d79b399102ea2ca796a6ba7e5
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-12 20:25:57 +0900

    Create basics.js

---
+++ basics.js
@@ -0,0 +1,23 @@
+var myName = "Beau" // Used throughout the program
+
+myName = 8
+
+let ourName = "hello" // use only withing the scope
+
+const pi = 3.14 // can't change
+
+var a;
+console.log(a)
+a = 7;
+var b = 2;
+
+b=a;
+
+console.log(a)
+
+console.log(a);
+var myStr = 'Hey my name is "mohil"'
+myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
+myStrLength = myStr.length;
+console.log(myStr);
+console.log(myStrLength);
