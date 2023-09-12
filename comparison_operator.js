873e40c66e287fd78212d10b07208a48f3d74328
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-12 21:51:48 +0900

    Create comparison_operator.js

---
+++ comparison_operator.js
@@ -0,0 +1,15 @@
+// Comparison operator
+
+let comp1 = 6;
+let comp2 = 7;
+let comp3 = "6";
+console.log(comp1 == comp2);
+console.log(comp1 != comp2);
+console.log(comp1 === comp2); // checks value and type
+
+console.log(comp1 == comp3);
+console.log(comp1 != comp3);
+console.log(comp1 === comp3);
+console.log(comp1 !== comp3);
+
+
