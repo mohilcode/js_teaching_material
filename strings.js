aecefea7f603a1bd414658098f5491b7307f3a39
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-13 02:04:57 +0900

    Create strings.js

--- strings.js
+++ strings.js
@@ -11,3 +11,33 @@ let boy2 = "Nikhil"
 let sentence = `boy 1 is a friend of boy2`
 let sentence1 = `${boy1} is a friend of ${boy2}`
 console.log(sentence1)
+
+let first = "helloboy"
+
+console.log(first.toUpperCase())
+console.log(first.toLowerCase())
+
+console.log(first.slice(2, 4))
+console.log(first.slice(2)) // print from 2 to end
+console.log(first.replace("hello","bye"))
+let second = "byegirl"
+console.log(first.concat(second))
+console.log(first) // String is immutable, it can't be changed
+let friend2 = "         meena             "
+console.log(friend2.trim())
+
+console.log("har\"".length)
+
+const word = "friend"
+
+console.log(sentence.includes(word) ? 'is' : 'is not');
+
+let statement = "Please give me Rs 1000"
+
+for(let i = 0; i < statement.length; ++i) {
+	if (!isNaN(statement[i])){
+		console.log(statement[i]);// will print individually tho
+	}
+}
+
+
