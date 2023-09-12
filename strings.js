a243c17ba0646ea2d776c9596428e6281b668808
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-13 01:28:38 +0900

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,13 @@
+let name = "harry"
+console.log(name.length)
+let friend = 'Prakash'
+console.log(friend)
+
+// Template Literals
+
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+//Nikhil is a friend of Pramod
+let sentence = `boy 1 is a friend of boy2`
+let sentence1 = `${boy1} is a friend of ${boy2}`
+console.log(sentence1)
