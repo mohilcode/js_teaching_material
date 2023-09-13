6e9d9067b9c1ee02acb2a44265123515e5553503
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-13 22:45:22 +0900

    Create DOM_BOM.js

---
+++ DOM_BOM.js
@@ -0,0 +1,9 @@
+//Window objects represents browser window and provides methods to control it. It is a global  object
+console.log(window)
+//DOM reprsents the page content as a javascript object
+console.log(document)
+console.log(document.body) // page body as js object
+document.body.style.background = "yellow" // You would want it incase user does something and based on that you want to change something
+//BOM represent additional objects provided by the broweser(host environment)
+alert("Harry")
+// location.href = "https://google.com" (for redirection)
