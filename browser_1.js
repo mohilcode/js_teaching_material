ae5b823d5a4863fd341c1c1734fd331910a0b01c
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-13 22:27:40 +0900

    Create browser_1.js

---
+++ browser_1.js
@@ -0,0 +1,9 @@
+console.log(console)
+console.error("Error bro")
+console.assert(5> 52) // assertion failed
+obj = {a:1, b:2, c:3}
+console.table(obj) //shows a tabular form
+console.warn("Aisa mat kar bsdk")
+console.info("you might be okay") //similar o condole.log
+console.time("a") // timer starts for a
+console.timeEnd("a") // Timer ends for a, it shows how much time a was active
