825eb3d6620e102e0afa6298505a9b771c4f0843
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-13 22:34:35 +0900

    Create alert_prompt_confirm.js

---
+++ alert_prompt_confirm.js
@@ -0,0 +1,6 @@
+alert("Hoooolllaaaa") //invokes a mini windows
+let a = prompt("Enter a here", "578") // second argument is the default value
+a = Number.parseInt(a);
+let write = confirm("pakka?")
+document.write(a)
+// Not used much because script is paused
