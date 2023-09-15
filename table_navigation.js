a60fa2d272ef54a05c439d20ea723ef549e52af6
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-15 19:41:18 +0900

    Create table_navigation.js

---
+++ table_navigation.js
@@ -0,0 +1,8 @@
+let t  = document.body.firstElementChild.firstElementChild;
+console.log(t);
+console.log(t.rows); // collecton of tr elements
+console.log(t.caption); // refernec to caption tag
+console.log(t.tHead); // refernce to thead
+console.log(t.tHead.firstElementChild); // 
+console.log(t.tBodies);
+console.log(t.rows[1].rowIndex)
