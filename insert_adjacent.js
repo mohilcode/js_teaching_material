f18011c9e1f03ab3edd2fd9a9bb952ae2456fde3
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-20 23:35:24 +0900

    Create insert_adjacent.js

---
+++ insert_adjacent.js
@@ -0,0 +1,4 @@
+first.insertAdjacentHTML('beforebegin', '<li>beforebegin</li>');
+first.insertAdjacentHTML('beforeend', '<li>beforeend</li>');
+first.insertAdjacentHTML('afterbegin', '<li>afterbegin</li>');
+first.insertAdjacentHTML('afterend', '<li>afterend</li>');
