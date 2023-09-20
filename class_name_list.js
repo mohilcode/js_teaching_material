b1e66efdbcf063335d86426c074b5b5a522025c8
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-20 23:45:28 +0900

    Create class_name_list.js

---
+++ class_name_list.js
@@ -0,0 +1,7 @@
+first.className = "text-black red"
+first.classList.remove("red")
+first.classList.add("red")
+first.classList.toggle("red")
+first.classList.toggle("red")
+let a = first.classList.contains("red")
+console.log(a)
